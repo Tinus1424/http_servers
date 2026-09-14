1cd8bc614e46c9a63814df880206c0c2cb7a2fd8
Tinus1424 <martijnqvtz@outlook.com>
2026-09-14 16:25:18 +0200

    excercise

---
+++ src/api/metrics.ts
@@ -0,0 +1,8 @@
+import type { Request, Response } from "express";
+import { config } from "../config.js";
+
+export async function handlerMetrics(_: Request, res: Response) {
+    res.send(`Hits: ${config.fileserverHits}`);
+}
+
+
