e3007e47c01f9cb7a88332ae64949bc03c54413d
Tinus1424 <martijnqvtz@outlook.com>
2026-09-15 16:00:02 +0200

    hi

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import express from "express";
 import { handlerReadiness } from "./api/readiness.js";
 import { handlerMetrics } from "./api/metrics.js";
 import { handlerReset } from "./api/reset.js";
+import { handlerChirpsValidate } from "./api/validate_chirp.js";
 import {
   middlewareLogResponse,
   middlewareMetricsInc,
@@ -12,11 +13,13 @@ const app = express();
 const PORT = 8080;
 
 app.use(middlewareLogResponse);
+app.use(express.json());
 app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 
-app.get("/healthz", handlerReadiness);
-app.get("/metrics", handlerMetrics);
-app.get("/reset", handlerReset);
+app.get("/api/healthz", handlerReadiness);
+app.get("/admin/metrics", handlerMetrics);
+app.post("/admin/reset", handlerReset);
+app.post("/api/validate_chirp", handlerChirpsValidate)
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
